4784089941884c517f959d7b0d13510cdcd78e27
Valerie Hudson <vhudsonphoto@gmail.com>
2018-03-16 01:31:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+var kittens = ["Milo","Otis","Garfield"]
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return name
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return name
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return name
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return name
+}
+function appendKitten(name) {
+  return [...kittens,name]
+}
+function prependKitten(name) {
+  return [name,...kittens]
+}
+function removeLastKitten() {
+  return [kittens.slice()]
+}
